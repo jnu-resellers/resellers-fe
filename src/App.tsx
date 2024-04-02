fcfc7d70fdaa3982f15e7adaae6e8fb62e7c3ab1
2yunseong <56749516+2yunseong@users.noreply.github.com>
2024-04-02 11:05:03 +0900

    feat: 초기 코드 및 스타일 초기화 (#2)
    * chore: styled components css 초기화
    
    * feat: 초기 코드 및 스타일 초기화 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>;
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './style/GlobalStyles';
+import theme from './style/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
+
+interface AppProps extends PropsWithChildren {}
+
+function App({ children }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/style/theme.ts
@@ -0,0 +1,9 @@
+const colors = {
+  // TODO: write colors based on own's design system
+};
+
+const fontSize = {
+  // TODO: write down font size based on own's design system
+};
+
+export default { colors, fontSize };

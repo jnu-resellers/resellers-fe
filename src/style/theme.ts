0dc7db2e39c957aca5b2e9d1652e44b49139800d
dxwwan <kkw1700@gmail.com>
2024-04-03 19:46:41 +0900

    feat: fontSize 테마 스타일 정의

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,7 +3,14 @@ const colors = {
 };
 
 const fontSize = {
-  // TODO: write down font size based on own's design system
+  title: '2.25rem',
+  subtitle: '2rem',
+  p1: '2.5rem',
+  p2: '1.75rem',
+  p3: '1.5rem',
+  p4: '1.25rem',
+  p5: '1rem',
+  p6: '0.75rem',
 };
 
 export default { colors, fontSize };

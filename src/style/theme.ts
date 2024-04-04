58496c8c9a459437dc531b5387746208299c46e1
dxwwan <kkw1700@gmail.com>
2024-04-04 10:49:13 +0900

    refactor: 폰트 스타일 chakraUI기반으로 변경

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,16 +1,35 @@
-const colors = {
-  // TODO: write colors based on own's design system
-};
+import { extendTheme } from "@chakra-ui/react";
 
-const fontSize = {
-  title: '2.25rem',
-  subtitle: '2rem',
-  p1: '2.5rem',
-  p2: '1.75rem',
-  p3: '1.5rem',
-  p4: '1.25rem',
-  p5: '1rem',
-  p6: '0.75rem',
-};
+const theme = extendTheme({
+  colors: {
+    // TODO: write colors based on own's design system
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+});
 
-export default { colors, fontSize };
+export default { theme };

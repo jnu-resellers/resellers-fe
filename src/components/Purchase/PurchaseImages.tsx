0110b1b64d6258f8b194535db48c7dbcd7efd3bf
dxwwan <kkw1700@gmail.com>
2024-04-07 15:26:37 +0900

    feat: 구매 이미지 컴포넌트 추가

---
+++ src/components/Purchase/PurchaseImages.tsx
@@ -0,0 +1,30 @@
+import { Flex, Box, Img } from "@chakra-ui/react";
+import { PurchaseDetailsProps } from "./Purchase";
+
+export interface PurchaseImagesProps {
+  products: PurchaseDetailsProps["products"];
+}
+
+export const PurchaseImages = ({ products }: PurchaseImagesProps) => {
+  return (
+    <Flex
+      flexDirection={"column"}
+      w={"100%"}
+      m={"2.25rem 2.25rem 0rem 8rem "}
+      maxW={"20rem"}
+      justify={"center"}
+    >
+      <Box marginBottom={"1rem"}>
+        {products.map((product) => (
+          <Img
+            key={product.id}
+            src={product.presignedUrl[0]}
+            alt={product.name}
+            width={"18rem"}
+            border={"1px solid black"}
+          />
+        ))}
+      </Box>
+    </Flex>
+  );
+};

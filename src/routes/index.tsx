1d221d642c0658860bbd5fdda778fb6bd09747f9
dxwwan <kkw1700@gmail.com>
2024-04-07 15:25:35 +0900

    feat: 구매 페이지 라우팅 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import { PurchasePage } from "../pages/Purchase.page";
 
 const routes = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: "purchase",
+        element: <PurchasePage />,
+      },
+    ],
   },
 ]);
 

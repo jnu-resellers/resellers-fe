76e286181cc8302f8ad06535c21b6e3d3fbd810d
dxwwan <kkw1700@gmail.com>
2024-04-07 15:29:29 +0900

    fix: 빌드오류(import) 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { PropsWithChildren } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { PurchaseDetailsProps } from "../components/Purchase/Purchase";
-import { PurchaseImagesProps } from "../components/Purchase/PurchaseImages";
 
 export const useProducts = () => {
   //default 값은 첫번째 product

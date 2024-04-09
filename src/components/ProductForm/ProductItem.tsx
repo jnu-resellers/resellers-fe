9c72f21e5f94d204d4caab7a4103846bc9f2b14c
2yunseong <dbsdltjd123@gmail.com>
2024-04-09 19:36:27 +0900

    feat: 등록된 상품 리스트 표현

---
+++ src/components/ProductForm/ProductItem.tsx
@@ -0,0 +1,51 @@
+import { Product } from '@/hooks/ProductForm/useProducts';
+import { Box, Divider, Flex, Heading, Text, theme } from '@chakra-ui/react';
+
+interface ProductItemProps {
+  product: Product;
+}
+
+interface ProductItemFieldProps {
+  title: string;
+  content: string;
+}
+
+const ProductItemField = ({ title, content }: ProductItemFieldProps) => {
+  return (
+    <Box w="100%" mb={6}>
+      <Heading as="h4" size="sm" mb={3}>
+        {title}
+      </Heading>
+      <Text>{content}</Text>
+    </Box>
+  );
+};
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <>
+      <Flex w="100%" justify="space-between">
+        {!product.imgFileNames.length ? (
+          <Box w="250px" h="170px" backgroundColor={theme.colors.gray[300]} />
+        ) : (
+          <img src="" />
+        )}
+
+        <Box w="50%">
+          <ProductItemField title="상품명" content={product.name} />
+          <ProductItemField
+            title="가격"
+            content={`${product.price.toLocaleString()}원`}
+          />
+          <ProductItemField
+            title="설명"
+            content={product.description || '이 상품에 대한 설명이 없습니다.'}
+          />
+        </Box>
+      </Flex>
+      <Divider />
+    </>
+  );
+};
+
+export default ProductItem;
--- src/components/ProductForm/ProductList.tsx
+++ src/components/ProductForm/ProductList.tsx
@@ -1,15 +1,27 @@
-import { Center, Divider, Text, theme } from '@chakra-ui/react';
+import { Divider, Text, VStack, theme } from '@chakra-ui/react';
 import SectionTitle from './SectionTitle';
+import { Product } from '@/hooks/ProductForm/useProducts';
+import ProductItem from './ProductItem';
 
-const ProductList = () => {
+interface ProductListProps {
+  products: Product[];
+}
+const ProductList = ({ products }: ProductListProps) => {
   return (
     <>
       <SectionTitle title="판매 상품" />
-      <Center my={10}>
-        <Text size="xl" color={theme.colors.gray[600]}>
-          상품을 등록해주세요!
-        </Text>
-      </Center>
+      <VStack w="100%" spacing={12} my={10}>
+        {!products.length && (
+          <Text size="xl" color={theme.colors.gray[600]}>
+            아래에서 상품을 등록해주세요!
+          </Text>
+        )}
+
+        {!!products.length &&
+          products.map((product) => (
+            <ProductItem key={product.name} product={product} />
+          ))}
+      </VStack>
       <Divider mb={5} />
     </>
   );

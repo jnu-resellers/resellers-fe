49a57177367a1dc15c0d8cc462038a3293fdcd16
mlnwns <mlnwns@naver.com>
2024-04-10 17:22:48 +0900

    feat: 메인 페이지 구현

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainFeed from '@/components/MainFeed';
+import Header from '@/components/Header';
+import PageLayout from '@/layouts/PageLayout';
+
+const MainPage = () => {
+  return (
+    <PageLayout>
+      <Header />
+      <MainFeed />
+    </PageLayout>
+  );
+};
+
+export default MainPage;

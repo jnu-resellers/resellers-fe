9caa162eb92cecd11fb247eb6e8074ce1c57e075
mlnwns <mlnwns@naver.com>
2024-04-10 17:19:56 +0900

    feat: 메인 페이지 라우터 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import ProductFormPage from '../pages/ProductFormPage';
+import MainPage from '@/pages/MainPage';
 
 const routes = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const routes = createBrowserRouter([
       {
         path: 'product-form',
         element: <ProductFormPage />,
+      },
+      {
+        path: '',
+        element: <MainPage />,
       },
     ],
   },

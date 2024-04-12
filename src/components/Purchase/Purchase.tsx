dc3e83a79e5f37a57e0aedc2f13d63e6e4e02ed6
dxwwan <kkw1700@gmail.com>
2024-04-12 14:32:44 +0900

    feat: 이미지를 클릭하면 해당 id상품만 보여주는 기능 추가

--- src/components/Purchase/Purchase.tsx
+++ src/components/Purchase/Purchase.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Flex, Text, Divider } from "@chakra-ui/react";
-import { PurchaseImages } from "./PurchaseImages";
-import { PurchaseDetails } from "./PurchaseDetails";
+import { Box, Button, Flex, Text, Divider, theme } from '@chakra-ui/react';
+import { PurchaseImages } from './PurchaseImages';
+import { PurchaseDetails } from './PurchaseDetails';
+import { usePurchase } from '@/hooks/purchase/usePurchase';
 
-export interface PurchaseDetailsProps {
+export interface PurchaseProps {
   title: string;
   writer: string;
   products: {
@@ -15,34 +16,40 @@ export interface PurchaseDetailsProps {
   error: string | null;
 }
 
-const CATEGORY = "요식업";
-const RESPONSE: PurchaseDetailsProps = {
-  title: "기자재 구매",
-  writer: "김기자",
+const CATEGORY = '요식업';
+const RESPONSE: PurchaseProps = {
+  title: '기자재 구매',
+  writer: '김기자',
   products: [
     {
       presignedUrl: [
-        "https://www.lge.co.kr/kr/images/refrigerators/md10039827/gallery/medium02.jpg",
+        'https://www.lge.co.kr/kr/images/refrigerators/md10039827/gallery/medium02.jpg',
       ],
       id: 1,
-      name: "냉장고",
+      name: '냉장고',
       price: 100000,
-      description: "냉장고입니다.",
+      description: '냉장고입니다.',
     },
     {
       presignedUrl: [
-        "https://img.danawa.com/prod_img/500000/416/518/img/13518416_1.jpg?_v=20220517155823",
+        'https://img.danawa.com/prod_img/500000/416/518/img/13518416_1.jpg?_v=20220517155823',
       ],
       id: 2,
-      name: "전자레인지",
+      name: '전자레인지',
       price: 50000,
-      description: "전자레인지입니다.",
+      description: '전자레인지입니다.',
     },
   ],
   error: null,
 };
 
 export const Purchase = () => {
+  const { selectedProductId, onClickProduct } = usePurchase();
+
+  const selectedProduct = RESPONSE.products.filter(
+    (product) => product.id === selectedProductId,
+  );
+
   return (
     <Box w="100%">
       <Flex justifyContent="start" alignItems="center">
@@ -53,17 +60,20 @@ export const Purchase = () => {
       </Flex>
       <Divider orientation="horizontal" />
       <Flex flexDirection="row">
-        <PurchaseImages products={RESPONSE.products} />
+        <PurchaseImages
+          products={RESPONSE.products}
+          onClickImage={onClickProduct}
+        />
         <Box w="100%" mr="8rem">
           <PurchaseDetails
             title={RESPONSE.title}
             writer={RESPONSE.writer}
-            products={RESPONSE.products}
-            error={RESPONSE.error}
+            selectedProduct={selectedProduct[0]}
           />
           <Button
             display="flex"
-            colorScheme="orange"
+            bgColor={theme.colors.orange[200]}
+            color="white"
             w="16rem"
             h="4rem"
             fontSize="1.25rem"

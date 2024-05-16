5ef05e9ac5389abbb27815d63ec16b88caa8cab7
mlnwns <mlnwns@naver.com>
2024-05-17 00:07:17 +0900

    feat: 거래 정보 데이터 불러오는 로직 추가

--- src/apis/materials.ts
+++ src/apis/materials.ts
@@ -13,3 +13,30 @@ export const getMaterials = async (): Promise<GetMaterialRes[]> => {
 
   return response.data.response.materials;
 };
+
+interface GetTradeInformationRes {
+  productName: string;
+  itemType: string;
+  price: number;
+  description: string;
+  defect: string;
+  contact: string;
+}
+
+export const getTradeInformation =
+  async (): Promise<GetTradeInformationRes> => {
+    const response = await https.get('/board/material');
+
+    return response.data.response;
+  };
+
+interface GetSellerInformationRes {
+  contact: string;
+}
+
+export const getSellerInformation =
+  async (): Promise<GetSellerInformationRes> => {
+    const response = await https.get('/seller');
+
+    return response.data.response;
+  };

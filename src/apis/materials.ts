b0953bd2813e492cff151f6730018bfa0bb7efdc
dxwwan <silicagel6624@gmail.com>
2024-05-17 16:49:18 +0900

    fix: 상품 get요청의 타입변경

--- src/apis/materials.ts
+++ src/apis/materials.ts
@@ -1,6 +1,14 @@
 import https from './https';
 
 interface GetMaterialRes {
+  preSignedUrl: string;
+  id: number;
+  title: string;
+  itemType: string; // TODO: 유니온 타입으로 제한 필요
+  totalPrice: number;
+}
+
+interface GetProductRes {
   writer: string;
   product: {
     preSignedUrl: string[];
@@ -18,7 +26,7 @@ export const getMaterials = async (): Promise<GetMaterialRes[]> => {
   return response.data.response.materials;
 };
 
-export const getMaterial = async (id: number): Promise<GetMaterialRes> => {
+export const getMaterial = async (id: number): Promise<GetProductRes> => {
   const response = await https.get(`/board/materials/${id}`);
   return response.data.response;
 };

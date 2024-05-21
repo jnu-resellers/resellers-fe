4d6a9fa6cc63312c653a01562b8234ef3a824c35
mlnwns <mlnwns@naver.com>
2024-05-21 21:50:10 +0900

    fix: 빈 태그 삭제

--- src/components/Auction/AuctionTitle.jsx
+++ src/components/Auction/AuctionTitle.jsx
@@ -3,13 +3,11 @@ import { Box, Heading } from '@chakra-ui/react';
 const AuctionTitle = ({ title }) => {
   return (
     <div>
-      <>
-        <Box w="100%" mb={9}>
-          <Heading as="h2" size="xl">
-            {title}
-          </Heading>
-        </Box>
-      </>
+      <Box w="100%" mb={9}>
+        <Heading as="h2" size="xl">
+          {title}
+        </Heading>
+      </Box>
     </div>
   );
 };

a56197cfdd69ed11fd2e87bfb331f7ad77b1acbb
2yunseong <dbsdltjd123@gmail.com>
2024-05-22 19:21:44 +0900

    fix: change msw bypass image upload logic

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,7 @@
 export const generateImgCloudFrontUrl = (imgUrl: string) => {
   return `${import.meta.env.VITE_AWS_CLOUDFRONT_URL}/${imgUrl}`;
 };
+
+export const generateFullApiUrl = (path: string) => {
+  return `${import.meta.env.VITE_API_URL}${path}`;
+};

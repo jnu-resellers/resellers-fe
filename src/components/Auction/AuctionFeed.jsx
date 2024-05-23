2ba193a023ebd29a84c41cd0d1a4b85108506f46
mlnwns <mlnwns@naver.com>
2024-05-23 13:09:02 +0900

    style: 경매 데이터 CSS 수정

--- src/components/Auction/AuctionFeed.jsx
+++ src/components/Auction/AuctionFeed.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Flex, Text, Box } from '@chakra-ui/react';
 import styled from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
@@ -9,7 +10,47 @@ const AuctionFeed = ({ selectedCategory }) => {
     queryFn: getAuctionList,
   });
 
-  console.log(auctions);
+  const [timeLefts, setTimeLefts] = useState({});
+
+  useEffect(() => {
+    const calculateTimeLeft = (endAt) => {
+      const difference = +new Date(endAt) - +new Date();
+      let timeLeft = {};
+
+      if (difference > 0) {
+        timeLeft = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60),
+        };
+      } else {
+        timeLeft = {
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        };
+      }
+
+      return timeLeft;
+    };
+
+    const updateTimes = () => {
+      if (auctions) {
+        const newTimeLefts = {};
+        auctions.forEach((auction) => {
+          newTimeLefts[auction.id] = calculateTimeLeft(auction.endAt);
+        });
+        setTimeLefts(newTimeLefts);
+      }
+    };
+
+    updateTimes();
+    const timer = setInterval(updateTimes, 1000);
+
+    return () => clearInterval(timer);
+  }, [auctions]);
 
   if (status === 'error') return <>에러 상태</>;
   if (status === 'loading') return <>로딩 중 ...</>;
@@ -24,16 +65,31 @@ const AuctionFeed = ({ selectedCategory }) => {
         <Flex direction="column">
           {filteredAuctions && filteredAuctions.length > 0 ? (
             filteredAuctions.map((auction) => (
-              <Flex key={auction.id} mb={4}>
-                <ImageField
-                  src={auction.preSignedUrl || 'default-image-url'}
-                  alt={auction.productName}
-                />
+              <Flex key={auction.id} mb={8}>
+                <ImageField src={auction.preSignedUrl} />
                 <TextField>
-                  <Text>{auction.productName}</Text>
-                  <Text>{auction.price}</Text>
-                  <Text>{auction.itemType}</Text>
-                  <Text>{auction.bidCount}</Text>
+                  <Text mb="1" color="gray.500" fontSize="xl">
+                    {auction.itemType}
+                  </Text>
+                  <Text fontWeight="bold" fontSize="xl">
+                    {auction.productName}
+                  </Text>
+                  <S>
+                    <Text w="32%" mt="52" color="gray.500" fontSize="xl">
+                      남은 시간: {timeLefts[auction.id]?.days}일{' '}
+                      {timeLefts[auction.id]?.hours}시간{' '}
+                      {timeLefts[auction.id]?.minutes}분{' '}
+                      {timeLefts[auction.id]?.seconds}초
+                    </Text>
+                    <FlexContainer>
+                      <Text fontWeight="bold" fontSize="xl">
+                        입찰 {auction.bidCount}건
+                      </Text>
+                      <Text fontWeight="bold" fontSize="24">
+                        현재가 {auction.price.toLocaleString()}원
+                      </Text>
+                    </FlexContainer>
+                  </S>
                 </TextField>
               </Flex>
             ))
@@ -61,4 +117,16 @@ const TextField = styled.div`
   height: 23rem;
   width: 60rem;
   border: 0.01rem solid #aaa;
+  padding: 2rem;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 68%;
+`;
+const S = styled.div`
+  display: flex;
+  align-items: end;
 `;

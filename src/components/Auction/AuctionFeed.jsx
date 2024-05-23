822aebb28e1c2c2a0d55e47257a741ddadd54a5a
mlnwns <mlnwns@naver.com>
2024-05-24 02:02:07 +0900

    feat: AuctionFeed 컴포넌트에서 분리된 로직, 컴포넌트 호출

--- src/components/Auction/AuctionFeed.jsx
+++ src/components/Auction/AuctionFeed.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Flex, Text, Box } from '@chakra-ui/react';
-import styled from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
 import { getAuctionList } from 'src/apis/materials';
+import AuctionItem from './AuctionItem';
+import calculateTimeLeft from 'src/utils/calculateTimeLeft';
 
 const AuctionFeed = ({ selectedCategory }) => {
   const { data: auctions, status } = useQuery({
@@ -13,34 +14,11 @@ const AuctionFeed = ({ selectedCategory }) => {
   const [timeLefts, setTimeLefts] = useState({});
 
   useEffect(() => {
-    const calculateTimeLeft = (endAt) => {
-      const difference = +new Date(endAt) - +new Date();
-      let timeLeft = {};
-
-      if (difference > 0) {
-        timeLeft = {
-          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-          minutes: Math.floor((difference / 1000 / 60) % 60),
-          seconds: Math.floor((difference / 1000) % 60),
-        };
-      } else {
-        timeLeft = {
-          days: 0,
-          hours: 0,
-          minutes: 0,
-          seconds: 0,
-        };
-      }
-
-      return timeLeft;
-    };
-
     const updateTimes = () => {
       if (auctions) {
         const newTimeLefts = {};
         auctions.forEach((auction) => {
-          newTimeLefts[auction.id] = calculateTimeLeft(auction.endAt);
+          newTimeLefts[auction.auctionId] = calculateTimeLeft(auction.endAt);
         });
         setTimeLefts(newTimeLefts);
       }
@@ -48,7 +26,6 @@ const AuctionFeed = ({ selectedCategory }) => {
 
     updateTimes();
     const timer = setInterval(updateTimes, 1000);
-
     return () => clearInterval(timer);
   }, [auctions]);
 
@@ -65,37 +42,11 @@ const AuctionFeed = ({ selectedCategory }) => {
         <Flex direction="column">
           {filteredAuctions && filteredAuctions.length > 0 ? (
             filteredAuctions.map((auction) => (
-              <Flex key={auction.id} mb={8}>
-                <ImageField src={auction.preSignedUrl} />
-                <TextField>
-                  <Text mb="1" color="gray.500" fontSize="xl">
-                    {auction.itemType}
-                  </Text>
-                  <Text fontWeight="bold" fontSize="xl">
-                    {auction.productName}
-                  </Text>
-                  <S>
-                    <Text w="35%" mt="52" color="gray.500" fontSize="xl">
-                      남은 시간: {timeLefts[auction.id]?.days}일{' '}
-                      {timeLefts[auction.id]?.hours}시간{' '}
-                      {timeLefts[auction.id]?.minutes}분{' '}
-                      {timeLefts[auction.id]?.seconds}초
-                    </Text>
-                    <FlexContainer>
-                      <Text
-                        fontWeight="bold"
-                        fontSize="xl"
-                        color={auction.bidCount > 9 ? 'red.500' : 'black'}
-                      >
-                        입찰 {auction.bidCount}건
-                      </Text>
-                      <Text fontWeight="bold" fontSize="24">
-                        현재가 {auction.price.toLocaleString()}원
-                      </Text>
-                    </FlexContainer>
-                  </S>
-                </TextField>
-              </Flex>
+              <AuctionItem
+                key={auction.auctionId}
+                auction={auction}
+                timeLeft={timeLefts[auction.auctionId]}
+              />
             ))
           ) : (
             <Text mt="20" fontSize="xl">
@@ -109,28 +60,3 @@ const AuctionFeed = ({ selectedCategory }) => {
 };
 
 export default AuctionFeed;
-
-const ImageField = styled.img`
-  height: 23rem;
-  width: 18rem;
-  background-color: #cacaca;
-  object-fit: cover;
-`;
-
-const TextField = styled.div`
-  height: 23rem;
-  width: 60rem;
-  border: 0.01rem solid #aaa;
-  padding: 2rem;
-`;
-
-const FlexContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  width: 68%;
-`;
-const S = styled.div`
-  display: flex;
-  align-items: end;
-`;

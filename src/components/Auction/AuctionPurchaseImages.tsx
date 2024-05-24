fd9f8c14dc40a3a30df75baa5fc70e55b937b506
dxwwan <silicagel6624@gmail.com>
2024-05-24 10:42:42 +0900

    feat: 경매 입찰 이미지 컴포넌트 추가

---
+++ src/components/Auction/AuctionPurchaseImages.tsx
@@ -0,0 +1,33 @@
+import { Flex, Box, Img } from '@chakra-ui/react';
+
+export interface PurchaseImagesProps {
+  preSignedUrl: string[];
+}
+
+export const AuctionPurchaseImages = (preSignedUrl: PurchaseImagesProps) => {
+  return (
+    <Flex
+      flexDirection="row"
+      w="100%"
+      m="2.25rem 2.25rem 0rem 8rem "
+      maxW="20rem"
+      justify="center"
+    >
+      <Flex marginBottom="16rem">
+        {preSignedUrl.preSignedUrl.map((url, index) => (
+          <Img
+            key={index}
+            src={url}
+            alt="product"
+            w="100%"
+            h="100%"
+            maxW="20rem"
+            maxH="20rem"
+            borderRadius="1rem"
+            flexDirection="row"
+          />
+        ))}
+      </Flex>
+    </Flex>
+  );
+};

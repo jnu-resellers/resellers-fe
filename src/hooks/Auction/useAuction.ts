9a1e9cad890db4f37b4d21caf7b0ce2885a0e6d9
dxwwan <silicagel6624@gmail.com>
2024-05-28 02:20:11 +0900

    feat: 경매 남은 시간 계산 함수 추가

--- src/hooks/Auction/useAuction.ts
+++ src/hooks/Auction/useAuction.ts
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 export const useAuction = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [restTime, setRestTime] = useState('');
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -11,5 +12,32 @@ export const useAuction = () => {
     setIsModalOpen(false);
   };
 
-  return { isModalOpen, openModal, closeModal };
+  const restTimeChecker = (startAt: string, endAt: string) => {
+    const startAtDate = new Date(startAt);
+    const endAtDate = new Date(endAt);
+    const timeDifference = endAtDate.getTime() - startAtDate.getTime();
+    const millisecondsInSecond = 1000;
+    const millisecondsInMinute = millisecondsInSecond * 60;
+    const millisecondsInHour = millisecondsInMinute * 60;
+    const millisecondsInDay = millisecondsInHour * 24;
+    const days = Math.floor(timeDifference / millisecondsInDay);
+    const hours = Math.floor(
+      (timeDifference % millisecondsInDay) / millisecondsInHour
+    );
+    const minutes = Math.floor(
+      (timeDifference % millisecondsInHour) / millisecondsInMinute
+    );
+    const seconds = Math.floor(
+      (timeDifference % millisecondsInMinute) / millisecondsInSecond
+    );
+
+    const formatNumber = (num: number) => num.toString().padStart(2, '0');
+
+    const formattedTime = `${days}일 ${formatNumber(hours)}:${formatNumber(
+      minutes
+    )}:${formatNumber(seconds)}`;
+    return formattedTime;
+  };
+
+  return { isModalOpen, openModal, closeModal, restTimeChecker };
 };

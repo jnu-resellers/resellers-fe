cac2bf98d9201ce2e12a0504cc2ce3e155192375
dxwwan <77495584+dxwwan@users.noreply.github.com>
2024-05-29 21:54:48 +0900

    feat: 기자재 상세, 경매 상세 페이지 QA (#56)
    * feat: 캐러셀 asstes 추가
    
    * feat: 경매 입찰 페이지에 parameter id 추가
    
    * feat: 캐러셀 컴포넌트를 부모 컴포넌트에 통합
    
    * feat: 구매 상세 이미지 컴포넌트에 캐러셀 컴포넌트 통합
    
    * refactor: 버튼 스타일 통합
    
    * feat: 입찰 경고 문구 추가
    
    * refactor: 캐러샐 화살표 이미지를 모듈로 호출

--- src/components/Purchase/PurchaseImages.tsx
+++ src/components/Purchase/PurchaseImages.tsx
@@ -1,32 +1,45 @@
 import { Flex, Img } from '@chakra-ui/react';
 import { generateImgCloudFrontUrl } from '../../utils/url';
+import { PrevArrow, NextArrow } from '../Auction/AuctionPurchaseImages';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 export interface PurchaseImagesProps {
   fileNames: string[];
 }
 
 export const PurchaseImages = ({ fileNames }: PurchaseImagesProps) => {
+  const settings = {
+    dots: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
   return (
     <Flex
       flexDirection="row"
       w="100%"
-      m="2.25rem 2.25rem 0rem 8rem "
-      maxW="20rem"
+      m="2.25rem"
+      maxW="30rem"
       justify="center"
     >
-      <Flex marginBottom="16rem">
-        {fileNames.map((fileName) => (
+      <Slider {...settings}>
+        {fileNames.map((fileName: string) => (
           <Img
             key={fileName}
             src={generateImgCloudFrontUrl(fileName)}
             alt={fileName}
             w="100%"
             h="100%"
-            maxW="20rem"
-            maxH="20rem"
-            objectFit="cover"
+            maxW="30rem"
+            maxH="30rem"
           />
         ))}
-      </Flex>
+      </Slider>
     </Flex>
   );
 };

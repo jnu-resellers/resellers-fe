64d336cd663cc8b39f2cf57886268d546500fcbb
mlnwns <mlnwns@naver.com>
2024-06-07 21:25:53 +0900

    feat: 헤더 컴포넌트 반응형 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,23 @@
-import { Box } from '@chakra-ui/react';
-import { EditIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  useBreakpointValue,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  Image,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { EditIcon, HamburgerIcon } from '@chakra-ui/icons';
 import { BsFillHousesFill } from 'react-icons/bs';
 import { BsPersonCircle } from 'react-icons/bs';
 import { RiAuctionFill } from 'react-icons/ri';
 import Logo from '@/assets/logo.png';
-import styled from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
 import IconTextLink from './IconTextLink';
 import { useState } from 'react';
@@ -20,6 +33,8 @@ const Header = ({ showIconsAndTexts, onLogoClick }: HeaderProps) => {
   const [isNotLogin, setIsNotLogin] = useState<boolean>(
     !localStorage.getItem(LS_MEMBER_ID)
   );
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const onLogin = () => {
     navigate('/signin');
   };
@@ -30,46 +45,145 @@ const Header = ({ showIconsAndTexts, onLogoClick }: HeaderProps) => {
     navigate('/');
   };
 
+  const mb = useBreakpointValue({ base: 4, md: 0 });
+  const flexProps = useBreakpointValue({
+    base: { w: '100%', justifyContent: 'space-between', alignItems: 'center' },
+    md: {},
+  });
+
   return (
-    <Box mb={24} display="flex" justifyContent="space-between">
-      <Link to="/">
-        <LogoImg src={Logo} onClick={onLogoClick} />
-      </Link>
+    <Box
+      mb={{ base: 12, md: 24 }}
+      display="flex"
+      flexDirection={{ base: 'column', md: 'row' }}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Flex {...flexProps}>
+        <Link to="/">
+          <Image
+            src={Logo}
+            onClick={onLogoClick}
+            width={{ base: '12rem', md: '15rem' }}
+            height={{ base: '2rem', md: '2.5rem' }}
+            cursor="pointer"
+          />
+        </Link>
+        <IconButton
+          aria-label="메뉴 열기"
+          icon={<HamburgerIcon />}
+          fontSize="2rem"
+          display={{ base: 'block', md: 'none' }}
+          onClick={onOpen}
+          bg="transparent"
+          color="#FBD38D"
+          mb={1}
+          justifyContent="flex-end"
+        />
+      </Flex>
       {showIconsAndTexts && (
-        <Box display="flex" alignItems="center">
-          <IconTextLink
-            icon={BsPersonCircle}
-            text={isNotLogin ? '로그인' : '로그아웃'}
-            onClick={isNotLogin ? onLogin : onLogout}
-          />
-          <Box h={8} width="2px" bg="gray.600" mx={6} />
-          <IconTextLink
-            icon={EditIcon}
-            text="판매하기"
-            onClick={() => navigate('/product-form')}
-          />
-          <Box h={8} width="2px" bg="gray.600" mx={6} />
-          <IconTextLink
-            icon={RiAuctionFill}
-            text="경매"
-            onClick={() => navigate('/auction')}
-          />
-          <Box h={8} width="2px" bg="gray.600" mx={6} />
-          <IconTextLink
-            icon={BsFillHousesFill}
-            text="커뮤니티"
-            onClick={() => navigate('/community')}
-          />
-        </Box>
+        <>
+          <Box
+            display={{ base: 'none', md: 'flex' }}
+            flexDirection={{ base: 'column', md: 'row' }}
+            alignItems={{ base: 'flex-start', md: 'center' }}
+            mt={{ base: 4, md: 0 }}
+          >
+            <IconTextLink
+              icon={BsPersonCircle}
+              text={isNotLogin ? '로그인' : '로그아웃'}
+              onClick={isNotLogin ? onLogin : onLogout}
+              mb={mb}
+            />
+            <Box
+              h={8}
+              width="2px"
+              bg="gray.600"
+              mx={6}
+              display={{ base: 'none', md: 'block' }}
+            />
+            <IconTextLink
+              icon={EditIcon}
+              text="판매하기"
+              onClick={() => navigate('/product-form')}
+              mb={mb}
+            />
+            <Box
+              h={8}
+              width="2px"
+              bg="gray.600"
+              mx={6}
+              display={{ base: 'none', md: 'block' }}
+            />
+            <IconTextLink
+              icon={RiAuctionFill}
+              text="경매"
+              onClick={() => navigate('/auction')}
+              mb={mb}
+            />
+            <Box
+              h={8}
+              width="2px"
+              bg="gray.600"
+              mx={6}
+              display={{ base: 'none', md: 'block' }}
+            />
+            <IconTextLink
+              icon={BsFillHousesFill}
+              text="커뮤니티"
+              onClick={() => navigate('/community')}
+              mb={mb}
+            />
+          </Box>
+
+          <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton mt={1} />
+              <DrawerHeader>메뉴</DrawerHeader>
+              <DrawerBody>
+                <VStack spacing={4} align="flex-start">
+                  <IconTextLink
+                    icon={BsPersonCircle}
+                    text={isNotLogin ? '로그인' : '로그아웃'}
+                    onClick={isNotLogin ? onLogin : onLogout}
+                    mb={mb}
+                  />
+                  <IconTextLink
+                    icon={EditIcon}
+                    text="판매하기"
+                    onClick={() => {
+                      navigate('/product-form');
+                      onClose();
+                    }}
+                    mb={mb}
+                  />
+                  <IconTextLink
+                    icon={RiAuctionFill}
+                    text="경매"
+                    onClick={() => {
+                      navigate('/auction');
+                      onClose();
+                    }}
+                    mb={mb}
+                  />
+                  <IconTextLink
+                    icon={BsFillHousesFill}
+                    text="커뮤니티"
+                    onClick={() => {
+                      navigate('/community');
+                      onClose();
+                    }}
+                    mb={mb}
+                  />
+                </VStack>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </>
       )}
     </Box>
   );
 };
-
-const LogoImg = styled.img`
-  width: 15rem;
-  height: 2.5rem;
-  cursor: pointer;
-`;
 
 export default Header;
